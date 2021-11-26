8d9dcab05a2991bc6880f83c06e15aca97715c6e
Priscilla Silva <priscilla.sg91@gmail.com>
2021-11-26 13:17:02 -0800

    changed to template literals

--- triangles.js
+++ triangles.js
@@ -7,7 +7,7 @@ let height = 13;
 
 const triangleOneArea = (base * height)/2;
 
-console.log("triangleOne area equals " + triangleOneArea + " cm2");
+console.log(`triangleOne area equals  ${triangleOneArea} cm2`);
 
 //triangleTwo
 base = 16.5;
@@ -15,7 +15,7 @@ height = 20.3;
 
 const triangleTwoArea = (base * height)/2;
 
-console.log("triangleTwo area equals " + triangleTwoArea + " cm2");
+console.log(`triangleOne area equals  ${triangleTwoArea} cm2`);
 
 //Comparison
 
@@ -38,7 +38,7 @@ height = 20.3;
 
 const triangleThreeArea = (base * height)/2;
 
-console.log("triangleThree area equals " + triangleThreeArea + " cm2");
+console.log(`triangleOne area equals  ${triangleThreeArea} cm2`);
 
 //triangleFour
 
@@ -47,7 +47,7 @@ height = 16.5;
 
 const triangleFourArea = (base * height)/2;
 
-console.log("triangleFour area equals " + triangleFourArea + " cm2");
+console.log(`triangleOne area equals  ${triangleFourArea} cm2`);
 
 //Comparison
 if (triangleThreeArea > triangleFourArea) {
@@ -69,7 +69,7 @@ height = 5.6;
 
 const triangleFiveArea = (base * height)/2;
 
-console.log("triangleFive area equals " + triangleFiveArea + " cm2");
+console.log(`triangleOne area equals  ${triangleFiveArea} cm2`);
 
 //triangleSix
 
@@ -78,7 +78,7 @@ height = 12.4;
 
 const triangleSixArea = (base * height)/2;
 
-console.log("triangleSix area equals " + triangleSixArea + " cm2");
+console.log(`triangleOne area equals  ${triangleSixArea} cm2`);
 
 //Comparison
 
