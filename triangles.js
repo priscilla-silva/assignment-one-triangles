4754680b1ec6fcc125bfb7f41ca2f0d20a1a446e
Priscilla Silva <priscilla.sg91@gmail.com>
2021-11-26 11:58:51 -0800

    initial commit

---
+++ triangles.js
@@ -0,0 +1,16 @@
+//TriangleOne
+
+let base = 10;
+let height = 13;
+
+let TriangleOneArea = (base * height)/2;
+
+console.log("TriangleOne area equals " + TriangleOneArea + " cm2");
+
+//TriangleTwo
+base = 16.5;
+height = 20.3;
+
+let TriangleTwoArea = (base * height)/2;
+
+console.log("TriangleTwo area equals " + TriangleTwoArea + " cm2");
